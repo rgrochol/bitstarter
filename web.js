3550a40204f353232be21b74ae429acb58d2eeab
rgr <rgr@rgr.com>
2013-07-10 20:23:43 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+txt = buf.toString(fs.readFile('./index.html'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(txt);
 });
 
 var port = process.env.PORT || 5000;
